1ba6b6f2e03c5596638d1d21bdac2065c6f1659a
育如 林 <judy152534@gmail.com>
2021-05-07 16:21:37 +0800

    加入專案檔案。

---
+++ DAL(Data Access Layer)/DataContext/AppConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Data_Access_Layer_.DataContext
+{
+    class AppConfiguration
+    {
+        //constructor
+        public AppConfiguration()
+        {
+            var configBuilder = new ConfigurationBuilder();  //obtain configuration setting
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configBuilder.AddJsonFile(path,false);
+            var root = configBuilder.Build();
+            var appsetting = root.GetSection("ConnectionStrings: DefaultConnection");
+            sqlconnectionString = appsetting.Value;
+        }
+
+        public string sqlconnectionString { get; set; }
+    }
+}
---
+++ DAL(Data Access Layer)/DataContext/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Data_Access_Layer_.DataContext
+{
+    public class DatabaseContext : DbContext
+    {
+        public class OptionsBuild
+        {
+            public OptionsBuild()    //then add constructor to initiate & obtain app configuration
+            {
+                settings = new AppConfiguration();
+                opsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
+                opsBuilder.UseSqlServer(settings.sqlconnectionString);
+                dbOptions = opsBuilder.Options;
+            }
+            //first add variaable: use to configure the connection to database
+            public DbContextOptionsBuilder<DatabaseContext> opsBuilder { get; set; }  
+            public DbContextOptions<DatabaseContext> dbOptions { get; set; }
+            private AppConfiguration settings { get; set; }
+        }
+        public static OptionsBuild ops = new OptionsBuild();  //initite static instance . why??
+
+    }
+}
